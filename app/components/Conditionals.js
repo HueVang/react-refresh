bf2af901a5b9a56d82d1362701b5b3767f0edbe0
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-10-19 17:09:18 -0500

    Added prevent a component from rendering example.

--- app/components/Conditionals.js
+++ app/components/Conditionals.js
@@ -76,7 +76,48 @@ class Mailbox extends React.Component{
   }
 }
 
+// Preventing a component from rendering
+class WarningBanner extends React.Component{
+  render() {
+    if (!this.props.warn) {
+      return null;
+    }
+
+    return (
+      <div>
+        Warning!
+      </div>
+    );
+  }
+}
+
+class Page extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
+  }
+
+  render() {
+    return(
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
 module.exports = {
   LoginControl: LoginControl,
-  Mailbox: Mailbox
+  Mailbox: Mailbox,
+  Page: Page
 };
--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Clock, Toggle, Greeting} = require('./components/App');
-var {LoginControl, Mailbox} = require('./components/Conditionals');
+var {LoginControl, Mailbox, Page} = require('./components/Conditionals');
 
 const messages = ['react', 're: react', 're:re: react'];
 ReactDOM.render(
-  <Mailbox unreadMessages={messages}/>,
+  <Page />,
   document.getElementById('app')
 );
