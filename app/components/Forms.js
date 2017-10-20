b82fd3e62c5af99db8df4f71a8670630d968a671
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-10-20 16:15:06 -0500

    Added multiple values input handling.

--- app/components/Forms.js
+++ app/components/Forms.js
@@ -65,7 +65,98 @@ class Essay extends React.Component{
   }
 }
 
+// A select form example
+class FlavorForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value : event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite La Croix flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
+
+// Handling multiple inputs example
+class Reservations extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    // ES5 Equivalent
+    // var partialState = {};
+    // partialState[name] = value;
+    // this.setState(partialState);
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+          name="isGoing"
+          type="checkbox"
+          checked={this.state.isGoing}
+          onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+          name="numberOfGuests"
+          type="number"
+          value={this.state.numberOfGuests}
+          onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
 module.exports = {
   NameForm: NameForm,
-  Essay: Essay
+  Essay: Essay,
+  FlavorForm: FlavorForm,
+  Reservations: Reservations
 }
--- app/index.js
+++ app/index.js
@@ -3,12 +3,12 @@ var ReactDOM = require('react-dom');
 var {Clock, Toggle, Greeting} = require('./components/App');
 var {LoginControl, Mailbox, Page} = require('./components/Conditionals');
 var {NumberList} = require('./components/ListAndKeys');
-var {NameForm, Essay} = require('./components/Forms');
+var {NameForm, Essay, FlavorForm, Reservations} = require('./components/Forms');
 
 const messages = ['react', 're: react', 're:re: react'];
 const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render(
-  <Essay />,
+  <Reservations />,
   document.getElementById('app')
 );
