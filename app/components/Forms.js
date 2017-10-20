d424761db89ba288927f8e858b491e7b1e9c2ed3
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-10-20 15:45:46 -0500

    Added textarea component with example.

--- app/components/Forms.js
+++ app/components/Forms.js
@@ -1,5 +1,6 @@
 var React = require('react');
 
+// A form that alerts the name inside the input field once submitted
 class NameForm extends React.Component{
   constructor(props){
     super(props);
@@ -30,6 +31,41 @@ class NameForm extends React.Component{
   }
 }
 
+// A textarea example using React
+class Essay extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted :' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 module.exports = {
-  NameForm: NameForm
+  NameForm: NameForm,
+  Essay: Essay
 }
--- app/index.js
+++ app/index.js
@@ -3,12 +3,12 @@ var ReactDOM = require('react-dom');
 var {Clock, Toggle, Greeting} = require('./components/App');
 var {LoginControl, Mailbox, Page} = require('./components/Conditionals');
 var {NumberList} = require('./components/ListAndKeys');
-var {NameForm} = require('./components/Forms');
+var {NameForm, Essay} = require('./components/Forms');
 
 const messages = ['react', 're: react', 're:re: react'];
 const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render(
-  <NameForm />,
+  <Essay />,
   document.getElementById('app')
 );
